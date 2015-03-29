2faefba2b7c5d4cca1c9c2439cb7cfb5c7b932f2
amnah <amnah.dev@gmail.com>
2015-03-29 02:36:15 -0400

    refactor download path for script files

--- setup.sh
+++ setup.sh
@@ -16,13 +16,12 @@ doSetup=true
 doWebServer=true
 doVnc=false
 
+downloadPath="https://raw.github.com/amnah/vps-setup-script/master/"
 
 # mysql root password
 # get ready to input it, it will pop up
 
-# date
-#now=`date +%F_%H%M%S`
-# ------------ config -----------------
+# ------------ end config -----------------
 
 
 
@@ -31,23 +30,21 @@ if $doSetup ; then
     #echo -e "nameserver 8.8.8.8\nnameserver 4.2.2.2" > /etc/resolv.conf
 
     # update and
-    sudo apt-get update
-    sudo apt-get -y upgrade
+    apt-get update && apt-get -y upgrade
 
     # nano + other apps for add-apt-repository cmd
     # http://stackoverflow.com/a/16032073
-    sudo apt-get -y install nano python-software-properties software-properties-common
+    apt-get -y install nano python-software-properties software-properties-common
 
     # update time
     rm -f /etc/localtime
     ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
-    sudo apt-get -y install ntp
+    apt-get -y install ntp
     service ntp restart
 
     # bashrc
     mv ~/.bashrc ~/.bashrc.bak
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/.bashrc
-    mv .bashrc ~/.bashrc
+    wget ${downloadPath}files/.bashrc -O ~/.bashrc
     chmod 644 ~/.bashrc
 
     # prevent root login with password (ssh keys only)
@@ -61,7 +58,7 @@ if $doSetup ; then
     sed -i "s/Port 22/Port $sshPort/g" /etc/ssh/sshd_config
 
     # fail2ban and logwatch
-    sudo apt-get -y install fail2ban logwatch
+    apt-get -y install fail2ban logwatch
     sed -i "s/--output mail/--output mail --mailto $email --detail high/g" /etc/cron.daily/00logwatch
 
     # restart ssh and fail2ban services
@@ -75,11 +72,10 @@ fi
 if $doWebServer ; then
     # git php nginx mysql
     # http://www.howtoforge.com/installing-nginx-with-php5-and-php-fpm-and-mysql-support-lemp-on-ubuntu-12.04-lts
-    #sudo add-apt-repository -y ppa:git-core/ppa
-    sudo apt-get update
-    sudo apt-get -y purge apache2* libapache2*
-    sudo apt-get -y install git php5 php5-cli mysql-server mysql-client nginx php5-fpm php5-mysql php5-gd php5-imagick php5-mcrypt php5-memcache php-apc php5-curl curl
-    #sudo apt-get -y install php5-suhosin php5-intl php-pear php5-imap php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+    add-apt-repository -y ppa:git-core/ppa
+    apt-get update
+    apt-get -y purge apache2* libapache2*
+    apt-get -y install git php5 php5-cli mysql-server mysql-client nginx php5-fpm php5-mysql php5-gd php5-imagick php5-mcrypt php5-memcache php-apc php5-curl curl
     #nano /etc/php5/cli/conf.d/ming.ini # change "#" to ";"
 
     # fix up some configs
@@ -89,9 +85,9 @@ if $doWebServer ; then
     mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
     mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
     rm /etc/nginx/sites-enabled/default
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/nginx.conf  -O /etc/nginx/nginx.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_baseApps -O /etc/nginx/sites-available/_baseApps
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/sites-available/_common -O /etc/nginx/sites-available/_common
+    wget ${downloadPath}files/nginx.conf  -O /etc/nginx/nginx.conf
+    wget ${downloadPath}files/sites-available/_baseApps -O /etc/nginx/sites-available/_baseApps
+    wget ${downloadPath}files/sites-available/_common -O /etc/nginx/sites-available/_common
 
     # set up data dir
     mkdir -p /data/sites /data/logs
@@ -99,7 +95,7 @@ if $doWebServer ; then
     ln -s /etc/nginx/sites-available/ /data
     ln -s /etc/nginx/sites-enabled/ /data
     ln -s /etc/nginx/sites-available/_baseApps /etc/nginx/sites-enabled/_baseApps
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/example.site -O /data/example.site
+    wget ${downloadPath}files/example.site -O /data/example.site
 
     # download and install/move phpMyAdmin
     # note: file gets named "download"
@@ -108,15 +104,13 @@ if $doWebServer ; then
     rm -f download
     mv phpMyAdmin* /data
     ln -s /data/phpMyAdmin* /data/phpMyAdmin
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/config.inc.php -O /data/phpMyAdmin/config.inc.php
+    wget ${downloadPath}files/config.inc.php -O /data/phpMyAdmin/config.inc.php
 
     # setup default + phpMyAdmin logs in nginx
-    mkdir /data/logs/_
-    touch /data/logs/_/access.log
-    touch /data/logs/_/error.log
+    mkdir /data/logs/_ 
     mkdir /data/logs/phpMyAdmin
-    touch /data/logs/phpMyAdmin/access.log
-    touch /data/logs/phpMyAdmin/error.log
+    touch /data/logs/_/access.log /data/logs/_/error.log
+    touch /data/logs/phpMyAdmin/access.log /data/logs/phpMyAdmin/error.log
 
     # add logrotate to site logs and change rotation settings
     sed -i "s/*.log/*.log \/data\/logs\/*\/*.log/g" /etc/logrotate.d/nginx
@@ -126,12 +120,12 @@ if $doWebServer ; then
 
     # add nginx configurations for fail2ban
     # http://snippets.aktagon.com/snippets/554-how-to-secure-an-nginx-server-with-fail2ban
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/proxy.conf -O /etc/fail2ban/filter.d/proxy.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-auth.conf -O /etc/fail2ban/filter.d/nginx-auth.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-login.conf -O /etc/fail2ban/filter.d/nginx-login.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-noscript.conf -O /etc/fail2ban/filter.d/nginx-noscript.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/filter.d/nginx-dos.conf -O /etc/fail2ban/filter.d/nginx-dos.conf
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/jail.local.tmp -O /etc/fail2ban/jail.local.tmp
+    wget ${downloadPath}files/filter.d/proxy.conf -O /etc/fail2ban/filter.d/proxy.conf
+    wget ${downloadPath}files/filter.d/nginx-auth.conf -O /etc/fail2ban/filter.d/nginx-auth.conf
+    wget ${downloadPath}files/filter.d/nginx-login.conf -O /etc/fail2ban/filter.d/nginx-login.conf
+    wget ${downloadPath}files/filter.d/nginx-noscript.conf -O /etc/fail2ban/filter.d/nginx-noscript.conf
+    wget ${downloadPath}files/filter.d/nginx-dos.conf -O /etc/fail2ban/filter.d/nginx-dos.conf
+    wget ${downloadPath}files/jail.local.tmp -O /etc/fail2ban/jail.local.tmp
 
     # combine the tmp jail.local.tmp into the preconfigured jail.conf
     cat /etc/fail2ban/jail.conf /etc/fail2ban/jail.local.tmp > /etc/fail2ban/jail.local
@@ -147,8 +141,8 @@ if $doWebServer ; then
     #find /data/sites -type f -print0 | xargs -0 chmod 0644 # NOPE dont need this
 
     # clean up and download site.sh and backup.sh
-    wget https://raw.github.com/amnah/vps-setup-script/master/site.sh
-    wget https://raw.github.com/amnah/vps-setup-script/master/backup.sh
+    wget ${downloadPath}site.sh
+    wget ${downloadPath}backup.sh
     chmod 700 site.sh backup.sh
 
     # display message about site.sh
@@ -160,11 +154,11 @@ fi
 
 if $doVnc ; then
     # install tightvnc and xfce
-    sudo apt-get -y install tightvncserver xfce4 xfce4-goodies
+    apt-get -y install tightvncserver xfce4 xfce4-goodies
 
     # update scripts
     mkdir ~/.vnc
-    wget https://raw.github.com/amnah/vps-setup-script/master/files/xstartup  -O ~/.vnc/xstartup
+    wget ${downloadPath}files/xstartup  -O ~/.vnc/xstartup
     sed -i "s/#alias vnc/alias vnc/g" ~/.bashrc
 
     # display message about vnc
